73e71057adce66dd6173b5a0a73745cd87b55c3a
sebaGM10 <42596542+sebaGM10@users.noreply.github.com>
2026-02-19 18:50:28 -0500

    Update firebase.config.ts

--- src/app/shared/services/firebase.config.ts
+++ src/app/shared/services/firebase.config.ts
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCWx_g0nr-sgaIMEu_Mj2dGYQTU2NjoG70",
-  authDomain: "formulariodocentes-d010b.firebaseapp.com",
-  projectId: "formulariodocentes-d010b",
-  storageBucket: "formulariodocentes-d010b.firebasestorage.app",
-  messagingSenderId: "156062737688",
-  appId: "1:156062737688:web:d97bd8010216976ff0aa6d"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
